8e1d97be3bf0734b99c91dcafa292ba0495a8ce4
Cristina <kc.cristinadev@gmail.com>
2026-08-31 14:52:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Essenciais -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compatibilidade -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- SEO básico -->
+    <title>Login</title>
+    <meta name="description" content="Descrição curta da página para Google e compartilhamento.">
+    <meta name="keywords" content="html, css, javascript, site">
+    <meta name="author" content="Seu Nome">
+
+    <!-- Indexação -->
+    <meta name="robots" content="index, follow">
+
+    <!-- Tema navegador mobile -->
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Open Graph (redes sociais) -->
+    <meta property="og:title" content="Título da Página">
+    <meta property="og:description" content="Descrição para redes sociais.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://seusite.com">
+    <meta property="og:image" content="https://seusite.com/imagem.jpg">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Título da Página">
+    <meta name="twitter:description" content="Descrição para Twitter/X">
+    <meta name="twitter:image" content="https://seusite.com/imagem.jpg">
+
+    <!-- Ícone -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+<title>Projeto</title>
+<link rel="stylesheet" href="estilo/login.css?v=123">
+</head>
+
+
+<!-- Classe inicial do tema -->
+<body>
+<?php
+// Verifica se existe status e mensagem na URL para exibir alert
+if (isset($_GET['status']) && isset($_GET['msg'])) {
+    $classe = $_GET['status'] === 'sucesso' ? 'sucesso' : 'erro';
+ echo '
+<div class="mensagem '.$classe.'">'.htmlspecialchars($_GET['msg']).'</div>';
+}
+?>
+
+
+<!-- Botão que alterna tema -->
+ <button id="theme-toggle">☀️</button>   
+
+<div class="login-container">
+    <!-- Título -->
+    <h2>Bem-vindo(a)!</h2>
+    <p>Faça login para acessar o sistema.</p>
+
+    <!-- Área dos cards -->
+
+<form action="process/login.php" method="POST">
+    
+  <input type="email" name="email" placeholder="E-mail" required>
+  <input type="password" name="senha" placeholder="Senha" required>
+
+  <button type="submit">Entrar</button>
+</form>
+
+<p>
+  Não tem conta?
+  <a href="tela/cadastro.php">Cadastre-se</a>
+</p>
+
+</div>
+
+<script src="acoes/temas.js"></script>
+
+</body>
+
+
+</html>
---
+++ inicio.php
@@ -0,0 +1,167 @@
+<?php
+session_start();
+include = "../process/login.php";
+
+if (!isset($_SESSION['usuario']['id'])) {
+    header("Location: index.php");
+    exit;
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/inicio.css">
+    <title>Tela inicial</title>
+</head>
+
+<body>
+
+<?php include 'tela/menu.php'; ?>
+
+<header>
+    <h2>Olá <? $usuario["nome"]; ?></h2>
+    <p>Bem vindo ao sistema de gestão de desempenho de sua empresa</p>
+</header>
+
+<main>
+<main>
+
+<div class="opcoes">
+
+    <div class="card-opcao" onclick="abrirFormulario('produto')">
+        <div class="icone">+</div>
+        <h3>Adicionar Produto</h3>
+    </div>
+
+    <div class="card-opcao" onclick="abrirFormulario('admin')">
+        <div class="icone">+</div>
+        <h3>Adicionar Administrador</h3>
+    </div>
+
+</div>
+
+<div id="Calculadora">
+<div id="overlay" onclick="fecharFormulario()"></div>
+<form id="informacoes" style="display:none;">
+   
+    <button type="button" class="fechar" onclick="fecharFormulario()">
+    ✕
+</button>
+        <h3>Adicionar Produto</h3>
+
+    <div class="campo">
+        <label for="nomeDoProdutoPrincipal">Nome do Produto</label>
+        <input
+            type="text"
+            id="nomeDoProdutoPrincipal"
+            name="nomeDoProdutoPrincipal"
+            placeholder="Ex: Produto A"
+            required>
+    </div>
+
+    <div class="campo">
+        <label for="estoqueFisico">Estoque Físico</label>
+        <input
+            type="number"
+            id="estoqueFisico"
+            name="estoqueFisico"
+            min="0"
+            placeholder="Ex: 200"
+            required>
+    </div>
+
+    <div class="campo">
+        <label for="estoqueVirtual">Estoque Virtual</label>
+        <input
+            type="number"
+            id="estoqueVirtual"
+            name="estoqueVirtual"
+            min="0"
+            placeholder="Ex: 150"
+            required>
+    </div>
+
+
+    <div class="campo">
+        <label for="tempoRepo">Tempo de Reposição (dias)</label>
+        <input
+            type="number"
+            id="tempoRepo"
+            name="tempoRepo"
+            min="0"
+            placeholder="Ex: 5"
+            required>
+    </div>
+<div class="campo">
+    <label for="preco">Preço (unidade)</label>
+
+    <input
+        type="number"
+        id="preco"
+        name="preco"
+        min="0"
+        step="0.01"
+        placeholder="Ex: 2.00"
+        required>
+</div>
+
+    <div class="alinhar">
+
+
+        <button type="button" onclick="armazenarValor()" id="calcular">
+    Adicionar
+</button>
+    </div>
+
+</form>
+
+</div>
+
+</main>
+<script>
+   function abrirFormulario(tipo){
+
+    const produto = document.getElementById("informacoes");
+
+    if(tipo === "produto"){
+        produto.style.display = "grid";
+
+        produto.scrollIntoView({
+            behavior:"smooth",
+            block:"start"
+        });
+    }
+
+    if(tipo !== "produto") return;
+
+    document.getElementById("informacoes")
+        .classList.add("abrir");
+
+    document.getElementById("overlay")
+        .classList.add("abrir");
+}
+
+function fecharFormulario(){
+
+    document.getElementById("informacoes")
+        .classList.remove("abrir");
+
+    document.getElementById("overlay")
+        .classList.remove("abrir");
+}
+
+
+setTimeout(function(){
+    document.querySelector(".mensagem").remove();
+}, 3000);
+</script>
+<script src="acoes/calculos.js"></script>
+
+<script src="acoes/temas.js"></script>
+</body>
+</html>
